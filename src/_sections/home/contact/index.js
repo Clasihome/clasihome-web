b8c832556cc83eec2aa16570c365c01724f00de2
ivanovicsolaro <ivan_solaro3@hotmail.com>
2020-11-25 13:47:36 -0300

    antes de captcha

--- src/_sections/home/contact/index.js
+++ src/_sections/home/contact/index.js
@@ -4,15 +4,26 @@ import Sphere from "../../../_components/sphere"
 import { Form, Input, Row, Col, Spin } from "antd"
 import { Button } from "../../../_components/buttons"
 import { Fade, Bounce } from "react-reveal"
-import { LoadingOutlined, CheckCircleFilled } from "@ant-design/icons"
+import {
+  LoadingOutlined,
+  CheckCircleFilled,
+  CloseOutlined,
+  CloseCircleOutlined,
+} from "@ant-design/icons"
 import "./index.less"
 
 const { Item } = Form
 
 export default () => {
-  const [loading, setLoading] = useState(false)
+  const [form] = Form.useForm()
+  const [loading, setLoading] = useState({ state: false, label: "Enviar" })
   const [done, setDone] = useState(null)
 
+  const error = (
+    <div>
+      <span style={{ color: "red" }}>Error! intente mas tarde porfavor</span>
+    </div>
+  )
   const success = (
     <div>
       <CheckCircleFilled style={{ color: "#002438" }} />
@@ -20,25 +31,40 @@ export default () => {
     </div>
   )
 
-  const onFinish = async values => {
-    try {
-      setDone(null)
-      setLoading(true)
-      /*const options = {
-        headers: { "Content-type": "Application/json" },
-        body: JSON.stringify(values),
-      }
+  const onSendMail = values => {
+    setLoading({ state: true, label: "Enviando correo..." })
+    var myHeaders = new Headers()
+    myHeaders.append("Content-Type", "application/x-www-form-urlencoded")
 
-      const response = await fetch("https://google.com", options);
-      const result = await response.json();
-      console.log(result);*/
-      setTimeout(() => {
-        setLoading(false)
-        setDone(success)
-      }, 5000)
-    } catch (e) {
-      console.log(e)
+    var urlencoded = new URLSearchParams()
+    urlencoded.append("emailType", "ContactForm")
+    urlencoded.append("message", String(values.message))
+    urlencoded.append("email", values.email)
+    urlencoded.append("name", values.fullName)
+    urlencoded.append("company", values.bussines)
+    urlencoded.append("phone", values.phone)
+
+    var requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: urlencoded,
+      redirect: "follow",
     }
+
+    fetch(
+      `https://api.clasihome.com/pdf-and-email-service/email-send?${urlencoded}`,
+      requestOptions
+    )
+      .then(response => response.json())
+      .then(r => {
+        form.resetFields()
+        setLoading({ state: false, label: "Enviar" })
+        setDone(success)
+      })
+      .catch(() => {
+        setLoading({ state: false, label: "Error" })
+        setDone(error)
+      })
   }
 
   return (
@@ -70,9 +96,10 @@ export default () => {
         <Bounce bottom>
           <div className="form-main-cont">
             <Form
+              form={form}
               className="contact-form"
               layout="vertical"
-              onFinish={onFinish}
+              onFinish={onSendMail}
             >
               <Row gutter={[32, 16]} align="middle">
                 <Col
@@ -82,28 +109,67 @@ export default () => {
                   <h3>¿Tienes alguna consulta? con gusto te responderemos</h3>
                 </Col>
                 <Col xs={24} md={12}>
-                  <Item name="fullName" label="Nombre y apellido">
-                    <Input disabled={loading} />
+                  <Item
+                    name="fullName"
+                    label="Nombre y apellido"
+                    rules={[{ required: true, message: "Ingrese su nombre" }]}
+                  >
+                    <Input disabled={loading.state} />
                   </Item>
                 </Col>
                 <Col xs={24} md={12}>
-                  <Item name="phone" label="Teléfono">
-                    <Input disabled={loading} />
+                  <Item
+                    name="phone"
+                    label="Teléfono"
+                    rules={[{ required: true, message: "Ingrese su teléfono" }]}
+                  >
+                    <Input disabled={loading.state} />
                   </Item>
                 </Col>
                 <Col xs={24} md={12}>
-                  <Item name="bussines" label="Empresa">
-                    <Input disabled={loading} />
+                  <Item
+                    name="bussines"
+                    label="Empresa"
+                    rules={[
+                      {
+                        required: true,
+                        message: "Ingrese el nombre de su empresa",
+                      },
+                    ]}
+                  >
+                    <Input disabled={loading.state} />
                   </Item>
                 </Col>
                 <Col xs={24} md={12}>
-                  <Item name="email" label="Email">
-                    <Input disabled={loading} />
+                  <Item
+                    name="email"
+                    label="Email"
+                    rules={[
+                      {
+                        required: true,
+                        message: "Ingrese su email",
+                      },
+                      {
+                        type: "email",
+                        message: "Ingrese un email correcto",
+                      },
+                    ]}
+                  >
+                    <Input disabled={loading.state} />
                   </Item>
                 </Col>
                 <Col xs={24}>
-                  <Item name="message" label="Mensaje">
-                    <Input.TextArea rows={4} disabled={loading} />
+                  <Item
+                    name="message"
+                    label="Mensaje"
+                    rules={[
+                      {
+                        required: true,
+                        message: "Ingrese su mensaje",
+                      },
+                    ]}
+                  >
+                    <Input.TextArea rows={4} disabled={loading.state} />
                   </Item>
                 </Col>
                 <Col xs={24} md={12}>
@@ -112,7 +178,7 @@ export default () => {
                       type="primary"
                       htmlType="submit"
                       block
-                      disabled={loading}
+                      disabled={loading.state}
                     >
                       Enviar
                     </Button>
@@ -125,9 +191,10 @@ export default () => {
                       display: "flex",
                       justifyContent: "center",
                       alignItems: "center",
+                      textAlign: "left",
                     }}
                   >
-                    {loading && (
+                    {loading.state && (
                       <Spin
                         indicator={
                           <LoadingOutlined style={{ color: "#dc314f" }} />
